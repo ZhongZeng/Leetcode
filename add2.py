1d74f282e98e3bf777b7e642a95cae98424ea5f8
Zhong Zeng <zengzhongzju@qq.com>
2017-03-03 10:42:26 -0500

    Add files via upload

---
+++ add2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Leetcode 2. Add 
+
+Created on Sat Dec 24 18:01:56 2016
+
+@author: Michael
+"""
+
---
+++ revstr344.py
@@ -0,0 +1,11 @@
+# Leetcode 344. Reverse String
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        rev = s[::-1];
+        
+        return rev;
+        
