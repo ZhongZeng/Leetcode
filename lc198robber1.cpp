224d9fc0c7a854e972f922846655b45e2864c8e5
Zhong Zeng <zengzhongzju@qq.com>
2017-04-18 22:33:13 -0400

    Add files via upload

---
+++ lc198robber1.cpp
@@ -0,0 +1,26 @@
+
+
+/*
+Leetcode 198. House Robber
+You are here! 
+Your runtime beats 2.68% of cpp submissions. 3 ms
+*/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        /*O(n) time, O(1) space*/
+        int maxlst=0, maxsd=0, temp;
+        /* the max value of the vector including last element (w/ or w/t the last element) 
+         , the max value of the vector excluding the last element (w/t the last element)*/
+        for(vector<int>::iterator it=nums.begin(); it!=nums.end(); it++){
+            temp = maxsd;
+            maxsd = maxlst;
+			maxlst = max(temp+*it, maxlst);
+            // cout<<maxlst<<" "<<maxsd<<endl;
+        }
+        return max(maxlst, maxsd);
+    }
+};
+
+
---
+++ lc198robber2.cpp
@@ -0,0 +1,29 @@
+
+
+/*
+Leetcode 198. House Robber
+You are here! 
+Your runtime beats 68.76% of cpp submissions. 0 ms
+This solution is very close to my solution 1.
+The only difference is the use of temp. 
+However, their running time is signnificantly different,
+according to Leetcode online judge. 
+*/
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        /*O(n) time, O(1) space*/
+        int maxlst=0, maxsd=0, temp;
+        /* the max value of the vector including last element (w/ or w/t the last element) 
+         , the max value of the vector excluding the last element (w/t the last element)*/
+        for(vector<int>::iterator it=nums.begin(); it!=nums.end(); it++){
+            temp = maxlst;
+			maxlst = max(maxsd+*it, maxlst);
+			maxsd = temp;
+            // cout<<maxlst<<" "<<maxsd<<endl;
+        }
+        return max(maxlst, maxsd);
+    }
+};
+
