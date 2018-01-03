8fc73edb005477a7ccdcfca19938768e1c59f587
Zhong Zeng <zengzhongzju@qq.com>
2018-01-02 23:54:05 -0500

    Add files via upload

---
+++ lc717bitChar.cpp
@@ -0,0 +1,41 @@
+
+
+/*
+Leetcode 717. 1-bit and 2-bit Characters
+
+Related Topics 
+Array
+Similar Questions 
+Gray Code
+
+Next challenges: Gray Code
+
+Test Cases:
+[1,0,0]
+[1, 1, 1, 0]
+[1,1,0,1,1,0]
+[1,1,0,0,1,1,0]
+[1,0,0,1,1,0]
+
+Runtime: 3 ms
+Your runtime beats 62.85 % of cpp submissions.
+
+*/
+
+class Solution {
+public:
+    bool isOneBitCharacter(vector<int>& bits) {
+        // 0, 10, 11
+        // The given string will always end with a zero. This makes sure the given string can alwasy be decoded. 
+        // Given a vaild string, there is only one way to decode it. 
+        // Return whether the last character must be a one-bit character or not.
+        // DP; O(n) time; could avoid travsing all elements
+        if(bits.size()<2)   return true;
+        for(int i=bits.size()-2; -1<i; i--){
+            if(0<bits[i]<1){// ==0
+                return (bits.size()-i-2)%2!=1;
+            }
+        }
+        return bits.size()%2!=0;
+    }
+};
