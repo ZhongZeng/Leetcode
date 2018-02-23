71adbcc44a794fd5c974483fb87f58cbc62cb10c
Zhong Zeng <zengzhongzju@qq.com>
2018-02-23 00:21:51 -0500

    Add files via upload

--- lc422vldWdSq.cpp
+++ lc422vldWdSq.cpp
@@ -15,11 +15,32 @@ Test Cases:
 [  "ball",  "area",  "read",  "lady"]
 ["ball","asee","let","lep"]
 
-Runtime: 18 ms
+Runtime: 16 ms
 Your runtime beats 23.73 % of cpp submissions.
 
 */
+ 
 
+class Solution {
+public:
+    bool validWordSquare(vector<string>& words) {
+        for(int i=0; i<words.size(); i++){
+            for(int j=0; j<words[i].size(); j++){
+                if(words.size()-1<j||words[j].size()-1<i||words[i][j]!=words[j][i]){
+                    //cout<<i<<","<<j<<endl;
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
+/*
+// This should fail, but it passes.
+// Runtime: 18 ms
+// Your runtime beats 23.73 % of cpp submissions.
+ 
 class Solution {
 public:
     bool validWordSquare(vector<string>& words) {
@@ -34,3 +55,30 @@ public:
         return true;
     }
 };
+*/
+
+/*
+// Runtime: 24 ms
+// Your runtime beats 6.78 % of cpp submissions.
+class Solution {
+public:
+    bool validWordSquare(vector<string>& words) {
+        int i, j;
+        for( i=0; i<words.size(); i++){
+            for( j=0; j<i&&j<words[i].size(); j++){
+                if(words.size()-1<j||words[j].size()-1<i){
+                    //cout<<i<<","<<j<<endl;
+                    return false;
+                }
+            }
+            while( ++j<words[i].size() ){
+                if(words.size()-1<j||words[j].size()<i||words[i][j]!=words[j][i]){
+                    //cout<<i<<","<<j<<endl;
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+*/
