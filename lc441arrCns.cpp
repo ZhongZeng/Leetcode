f9d3fb938617a98b03769b87884cc0c2426482a9
Zhong Zeng <zengzhongzju@qq.com>
2018-03-10 09:57:01 -0500

    Add files via upload

---
+++ lc441arrCns.cpp
@@ -0,0 +1,34 @@
+
+/*
+Leetcode 441. Arranging Coins
+
+Companies 
+GoDaddy 
+Related Topics 
+Math, Binary Search
+
+Next challenges: Search in Rotated Sorted Array II, 
+Russian Doll Envelopes, Swim in Rising Water
+
+Test Cases:
+5
+8
+100
+1804289383
+
+*/
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        // Binary search 
+        if(n<2) return n;
+        int b=0, e=n, mid;// [b,e)
+        while(1<e-b){
+            mid=b+(e-b)/2;
+            if(n*1.0/mid<(1+mid)/2.0)   e=mid;
+            else    b=mid;
+        }
+        return b;
+    }
+};
