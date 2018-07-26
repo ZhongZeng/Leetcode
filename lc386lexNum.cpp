8d4a27f8f4915f4aef2860bd5b0b7b7bcdb49f5a
Zhong Zeng <zengzhongzju@qq.com>
2018-07-26 19:49:00 -0400

    Add files via upload

--- lc386lexNum.cpp
+++ lc386lexNum.cpp
@@ -41,3 +41,57 @@ public:
 		return ;
 	}
 };
+
+
+
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> rt;
+		if(n<1)	return rt;
+		
+		for( int i=1; i<10; i++)    dfs( i, n, rt);
+		
+		return rt;
+    }
+	
+	void dfs( int i, int& n, vector<int> & rt){// DFS, backtrack
+		rt.push_back(i);
+		
+		for( int j=0; j<10; j++){
+			if(i<=n)	dfs( i*10+j, n, rt);
+		}
+		
+		return ;
+	}
+};
+
+
+// Runtime: 252 ms
+// Your runtime beats 11.91 % of cpp submissions.
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> rt;
+		if(n<1)	return rt;
+        if(n<10){
+            for( int i=1; i<=n; i++)    rt.push_back(i);
+            return rt;
+        }
+		
+		for( int i=1; i<10; i++)    dfs( i, n, rt);
+		
+		return rt;
+    }
+	
+	void dfs( int i, int& n, vector<int> & rt){// DFS, backtrack
+		rt.push_back(i);
+		
+		for( int j=0, t; j<10; j++){
+            t=i*10+j;
+			if(t<=n)	dfs( t, n, rt);
+		}
+		
+		return ;
+	}
+};
