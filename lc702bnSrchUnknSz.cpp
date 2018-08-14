34c8ba6c2c17d06ffac80e76fe350d9c41282478
Zhong Zeng <zengzhongzju@qq.com>
2018-08-14 00:02:04 -0400

    Add files via upload

--- lc702bnSrchUnknSz.cpp
+++ lc702bnSrchUnknSz.cpp
@@ -30,7 +30,7 @@ class Solution {
 public:
     int search(const ArrayReader& reader, int target) {
         // array[-9999, 9999]; ArrayReader.get return 2147483647, if out of bounds 
-        int b=0, e=INT_MAX, mid;
+        int b=0, e=INT_MAX, mid;// actually unique array[-9999, 9999], e=20001
         
         while( b+1<e ){// [b,e) 
             mid=b+(e-b)/2;
@@ -41,3 +41,29 @@ public:
         return reader.get(b)!=target?-1:b;
     }
 };
+
+
+/*
+Runtime: 48 ms
+Your runtime beats 4.76 % of cpp submissions.
+
+// Forward declaration of ArrayReader class.
+class ArrayReader;
+
+class Solution {
+public:
+    int search(const ArrayReader& reader, int target) {
+        // array[-9999, 9999]; ArrayReader.get return 2147483647, if out of bounds 
+        int b=0, e=20001, mid;
+        
+        while( b+1<e ){// [b,e) 
+            mid=b+(e-b)/2;
+            if(target<reader.get(mid))  e=mid;
+            else    b=mid;
+        }
+        
+        return reader.get(b)!=target?-1:b;
+    }
+};
+
+*/
