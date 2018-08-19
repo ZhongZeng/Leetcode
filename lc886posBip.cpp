3005d5e32bf2b71189cedd158255326d7984537a
Zhong Zeng <zengzhongzju@qq.com>
2018-08-18 23:52:00 -0400

    Add files via upload

---
+++ lc886posBip.cpp
@@ -0,0 +1,74 @@
+
+/*
+Leetcode 886. Possible Bipartition (previously 890 during contest)
+
+Companies   
+Microsoft 
+Related Topics 
+Depth-first Search 
+
+Test Cases:
+4
+[[1,2],[1,3],[2,4]]
+3
+[[1,2],[1,3],[2,3]]
+5
+[[1,2],[2,3],[3,4],[4,5],[1,5]]
+
+Runtime: 176 ms
+
+*/
+
+class Solution {
+public:
+    bool possibleBipartition(int N, vector<vector<int>>& dislikes) {
+        // typical Bipartition 
+        if(dislikes.size()<1)   return true;
+        
+        bool b=true;
+        unordered_map<int,unordered_set<int>> um;
+        unordered_map<int,unordered_set<int>>::iterator umi;
+        vector<int> vt(N+1,0);
+
+        // Record Edges 
+        for( int i=1; i<=N; i++){
+            unordered_set<int> us;
+            um.emplace( i, us);
+        }
+        
+        for( int i=0; i<dislikes.size(); i++){
+            umi=um.find(dislikes[i][0]);
+            if(umi->second.find(dislikes[i][1])==umi->second.end())  umi->second.emplace(dislikes[i][1]);
+            umi=um.find(dislikes[i][1]);
+            if(umi->second.find(dislikes[i][0])==umi->second.end())  umi->second.emplace(dislikes[i][0]);
+        }
+        
+        // DFS
+        for( int i=1; i<=N; i++){
+            if(vt[i]==0){// guranteed to visit all connected part 
+                vt[i]=1;
+                b=dfs( i, vt, um );
+            }
+            if(!b)  return false;
+        }
+        
+        return b;
+    }
+    
+    bool dfs( int i, vector<int>& vt, unordered_map<int,unordered_set<int>>& um ){
+        bool b;
+        unordered_map<int,unordered_set<int>>::iterator umi=um.find(i);
+        
+        for( unordered_set<int>::iterator usi=umi->second.begin(); usi!=umi->second.end(); usi++){
+            if(vt[*usi]==0){
+                vt[*usi]=vt[i]*-1;
+                b=dfs( *usi, vt, um);
+                if(!b)  return false;
+            }else if(vt[*usi]==vt[i]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
