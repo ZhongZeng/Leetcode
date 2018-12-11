6cb892b9e1b7e479739eddaf5604fa5bf39311b0
Zhong Zeng <zengzhongzju@qq.com>
2018-12-10 21:04:52 -0500

    Add files via upload

--- lc647pldSubstr.cpp
+++ lc647pldSubstr.cpp
@@ -15,12 +15,30 @@ Test Cases:
 "aaa"
 
 Next challenges: Longest Palindromic Subsequence
-
-Runtime: 4 ms
-Your runtime beats 94.21 % of cpp submissions.
-
 */
 
+// Runtime: 4 ms, faster than 94.26% of C++ online submissions for Palindromic Substrings.
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int sum=0;
+
+        for( int i=0; i<s.size(); i++){
+            for( int j=i-1, k=i; -1<j&&k<s.size(); j--, k++){
+                if(s[j]==s[k])  sum++;
+                else    break;
+            }
+            for( int j=i, k=i; -1<j&&k<s.size(); j--, k++){
+                if(s[j]==s[k])  sum++;
+                else    break;
+            }        
+        }
+
+        return sum;        
+    }
+};
+
+// Runtime: 4 ms	Your runtime beats 94.21 % of cpp submissions.
 class Solution {
 public:
     int countSubstrings(string s) {
