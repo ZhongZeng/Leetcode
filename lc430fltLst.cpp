cf47302fadd8f5678117c4df23670617ee37e896
Zhong Zeng <zengzhongzju@qq.com>
2018-12-25 11:51:17 -0500

    Add files via upload

--- lc430fltLst.cpp
+++ lc430fltLst.cpp
@@ -1,5 +1,4 @@
 
-
 /*
 Leetcode 430. Flatten a Multilevel Doubly Linked List
 
@@ -14,12 +13,6 @@ Test Case:
 {"$id":"1","child":null,"next":{"$id":"2","child":null,"next":{"$id":"3","child":{"$id":"7","child":null,"next":{"$id":"8","child":{"$id":"11","child":null,"next":{"$id":"12","child":null,"next":null,"prev":{"$ref":"11"},"val":12},"prev":null,"val":11},"next":{"$id":"9","child":null,"next":{"$id":"10","child":null,"next":null,"prev":{"$ref":"9"},"val":10},"prev":{"$ref":"8"},"val":9},"prev":{"$ref":"7"},"val":8},"prev":null,"val":7},"next":{"$id":"4","child":null,"next":{"$id":"5","child":null,"next":{"$id":"6","child":null,"next":null,"prev":{"$ref":"5"},"val":6},"prev":{"$ref":"4"},"val":5},"prev":{"$ref":"3"},"val":4},"prev":{"$ref":"2"},"val":3},"prev":{"$ref":"1"},"val":2},"prev":null,"val":1}
 
 Next challenges: Reconstruct ItineraryThe Maze IIIRobot Room Cleaner
-
-Runtime: 24 ms
-Your runtime beats 98.96 % of cpp submissions.
-
-Runtime: 28 ms
-Your runtime beats 9.38 % of cpp submissions.
 
 */
 
@@ -42,6 +35,34 @@ public:
     }
 };
 */
+
+// Runtime: 24 ms, faster than 99.87% of C++ online submissions for Flatten a Multilevel Doubly Linked List.
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        // stack, equivalent to pre-order DFS; return root of single level, double linked list 
+        if(!head)   return head;
+        vector<Node*> vn;
+        Node* rt=head, * nd=head;
+        if(nd->next)    vn.push_back(nd->next);// first node
+        if(nd->child)   vn.push_back(nd->child);
+        nd->child=NULL;
+        
+        while(!vn.empty()){
+            vn.back()->prev=nd;
+            nd->next=vn.back();
+            nd=vn.back();
+            vn.pop_back();
+            if(nd->next)    vn.push_back(nd->next);
+            if(nd->child)   vn.push_back(nd->child);
+            nd->child=NULL;
+        }
+        return rt;
+    }
+};
+
+// Runtime: 24 ms	Your runtime beats 98.96 % of cpp submissions.
+// Runtime: 28 ms	Your runtime beats 9.38 % of cpp submissions.
 class Solution {
 public:
     Node* flatten(Node* head) {// this, child, next 
