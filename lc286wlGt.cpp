a78484d5afb9a62e7c62b6519172998547ddf890
Zhong Zeng <zengzhongzju@qq.com>
2019-01-04 19:07:31 -0500

    Add files via upload

--- lc286wlGt.cpp
+++ lc286wlGt.cpp
@@ -1,27 +1,68 @@
-
 /*
-
 Leetcode 286. Walls and Gates
 
-Companies 
-Google, Facebook
-Related Topics 
-Breadth-first Search
-Similar Questions 
-Surrounded Regions, Number of Islands, Shortest Distance from All Buildings
+Companies	Google, Facebook
+Related Topics	Breadth-first Search
+Similar Questions	Surrounded Regions, Number of Islands, Shortest Distance from All Buildings
 
 Next challenges: Shortest Distance from All Buildings
 
 Test Cases:
 [[2147483647,-1,0,2147483647],[2147483647,2147483647,2147483647,-1],[2147483647,-1,2147483647,-1],[0,-1,2147483647,2147483647]]
+[[2147483647,0,2147483647,2147483647,0,2147483647,-1,2147483647]]
 [[],[]]
 [[]]
-
-Runtime: 112 ms
-Your runtime beats 38.62 % of cpp submissions.
-
 */
 
+// Runtime: 116 ms, faster than 24.56% of C++ online submissions for Walls and Gates.
+class Solution {
+public:
+    void wallsAndGates(vector<vector<int>>& rooms) {
+		// BFS from all gates, O(m*n) time )
+        vector<int> vc, vd;
+		for( int i=0; i<rooms.size(); i++){
+			for( int j=0; j<rooms[i].size(); j++){
+				if(rooms[i][j]==0){
+					vc.push_back(i);
+					vc.push_back(j);
+				}
+			}
+		}
+		
+		for( int d=1; !vc.empty(); d++){
+			vd.clear();
+			for( int k=0, i, j; k<vc.size(); k+=2){
+				i=vc[k];
+				j=vc[k+1];
+				if(0<i&&d<rooms[i-1][j]){
+					rooms[i-1][j]=d;
+					vd.push_back(i-1);
+					vd.push_back(j);
+				}
+				if(0<j&&d<rooms[i][j-1]){
+					rooms[i][j-1]=d;
+					vd.push_back(i);
+					vd.push_back(j-1);
+				}
+				if(i<rooms.size()-1&&d<rooms[i+1][j]){
+					rooms[i+1][j]=d;
+					vd.push_back(i+1);
+					vd.push_back(j);
+				}
+				if(j<rooms[i].size()-1&&d<rooms[i][j+1]){
+					rooms[i][j+1]=d;
+					vd.push_back(i);
+					vd.push_back(j+1);
+				}
+			}
+			vc=vd;
+		}
+		
+		return ;
+    }
+};
+
+// Runtime: 112 ms	Your runtime beats 38.62 % of cpp submissions.
 class Solution {
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
