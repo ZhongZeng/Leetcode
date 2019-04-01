7497c8b9632e2f366360a6b1f2ef5434c86d7ab1
Zhong Zeng <zengzhongzju@qq.com>
2019-03-31 23:08:12 -0400

    Add files via upload

---
+++ lc1015smIntDvK.cpp
@@ -0,0 +1,34 @@
+/*
+1015. Smallest Integer Divisible by K
+
+Test Cases:
+1
+2
+3
+17
+
+Runtime: 36 ms
+Memory Usage: 15.3 MB
+
+https://leetcode.com/problems/smallest-integer-divisible-by-k/discuss/261226/
+*/
+
+class Solution {
+public:
+    int smallestRepunitDivByK(int K) {
+        // DP, math; K in [1,10^5]
+        int len, r;
+        unordered_set<int> um;
+        
+        for( len=1, r=1%K; ; len++){
+            if(r==0)    return len;
+            
+            if(um.find(r)!=um.end())    return -1;
+            else    um.emplace(r);
+            
+            r=(10*r+1)%K;
+        }
+        
+        return -1;
+    }
+};
---
+++ lc1016bnStr1N.cpp
@@ -0,0 +1,39 @@
+/*
+1016. Binary String With Substrings Representing 1 To N
+
+Next challenges: Text Justification, Mini Parser, Basic Calculator IV
+
+Test Cases:
+"0110"
+3
+"0110"
+4
+
+Runtime: 4 ms
+Memory Usage: 8.5 MB
+
+https://leetcode.com/problems/binary-string-with-substrings-representing-1-to-n/discuss/261229/
+*/
+
+class Solution {
+public:
+    bool queryString(string S, int N) {
+        // math +backtrack +bit; O(M*20+N) time; S.size() in [1,10^3]; N in [1,10^9]
+        vector<int> vc(N+1,0);// [1,N] ignoring 0 
+        int i, j, t, d;
+        
+        for( i=S.size()-1; -1<i; i--){
+            for( t=0, d=1, j=i; -1<j&&i-20<j; j--, d=d<<1){
+                if(S[j]=='1') t+=d;
+                if(t<=N)    vc[t]=1;
+                else    break;
+            }
+        }
+        
+        for( i=1; i<vc.size(); i++){
+            if(vc[i]==0)    return false;
+        }
+        
+        return true;
+    }
+};
