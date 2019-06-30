448b6e3a43efe55f6c73abc9612a94e382d9de33
Zhong Zeng <zengzhongzju@qq.com>
2019-06-29 23:50:11 -0700

    Add files via upload

--- lc1104pthZgBT.cpp
+++ lc1104pthZgBT.cpp
@@ -15,20 +15,19 @@ Memory Usage: 8.3 MB
 class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
-        // O(log(n)) time&space
         int i, p;
         vector<int> vb={1}, ve={1}, rt;
         
         while(ve.back()<label){
-            vb.push_back(vb.back()<<1);
-            ve.push_back((ve.back()<<1)+1);
+            vb.push_back(vb.back()<<1);// smallest element on each row
+            ve.push_back((ve.back()<<1)+1);// largest element on each row
         }
         
         p=vb.size()%2==0?vb.back()+ve.back()-label:label;
         for( i=vb.size()-1; 0<p; p=p>>1, i--){// (0<p) == (-1<i)
-            rt.push_back(i%2==0?p:vb[i]+ve[i]-p);
+            rt.push_back(i%2==0?p:vb[i]+ve[i]-p);// on even row: original number; on odds row: use opposite number, each pair of opposite numbers has the same sum
         }
-        reverse( rt.begin(), rt.end());
+        reverse( rt.begin(), rt.end());// previous iteration moves from bottom
         
         return rt;
     }
