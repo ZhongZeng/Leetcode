6c8cde8a0ab6d835b101c3ea32e4ca8e62334a70
Zhong Zeng <zengzhongzju@qq.com>
2020-01-13 23:18:55 -0800

    Add files via upload

--- lc1314mxBkSum.cpp
+++ lc1314mxBkSum.cpp
@@ -6,6 +6,8 @@ Related Topics	Dynamic Programming
 
 Runtime: 20 ms, faster than 99.51% of C++ online submissions for Matrix Block Sum.
 Memory Usage: 12.7 MB, less than 100.00% of C++ online submissions for Matrix Block Sum.
+Runtime: 32 ms, faster than 56.92% of C++ online submissions for Matrix Block Sum.
+Memory Usage: 12.6 MB, less than 100.00% of C++ online submissions for Matrix Block Sum.
 
 Next challenges:
 Frog Jump, Push Dominoes, Shortest Way to Form String
